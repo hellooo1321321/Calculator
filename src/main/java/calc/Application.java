415292f0449b1987185a682dd5e5e8e9b7e33cc8
yoonjae <realyoon77@gmail.com>
2024-03-06 14:55:40 +0900

    master

---
+++ src/main/java/calc/Application.java
@@ -0,0 +1,30 @@
+package calc;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // 메인
+        // 스캐너 이용 반복문 활용 1.2.3.4. 사칙연산 9. 프로그램종료
+        // 찬우
+
+
+
+    }
+
+    // 더하기 plus
+
+    // 뺴기 min
+
+    // 윤재
+
+    // 곱하기 multi
+
+    // 나누기 div
+
+    // 도아
+
+    // 나머지 mod
+
+    // 정민 try catch 활용
+}
