b78a18fd6b749b2b290f620f6fd028e2537894a9
222hojun <sciboviator@gmail.com>
2024-03-06 15:10:40 +0900

    정민 모드 메소드

--- src/main/java/calc/Application.java
+++ src/main/java/calc/Application.java
@@ -4,27 +4,18 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // 메인
-        // 스캐너 이용 반복문 활용 1.2.3.4. 사칙연산 9. 프로그램종료
-        // 찬우
-
-
 
     }
 
-    // 더하기 plus
 
-    // 뺴기 min
-
-    // 윤재
-
-    // 곱하기 multi
-
-    // 나누기 div
-
-    // 도아
 
     // 나머지 mod
+    public void mod(int first, int second) {
+
+        return first % second;
+
+    }
+
 
     // 정민 try catch 활용
 }
